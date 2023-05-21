7feee16fea21e84653af69664f5ac8654b24b825
Shailendra <91621557+shailendra-k1245@users.noreply.github.com>
2023-05-21 19:51:15 +0530

    added images and env variables

--- src/components/Docs.js
+++ src/components/Docs.js
@@ -48,10 +48,10 @@ export const Docs = ({ database }) => {
 
             <div className="docs-main">
                 <h1>Docs Clone</h1>
-                <button className="add-docs"
+                {/* <button className="add-docs"
                     onClick={handleOpen}>
                     Add a Document
-                </button>
+                </button> */}
                 <ModalComponent
                     open={open}
                     setOpen={setOpen}
@@ -60,22 +60,46 @@ export const Docs = ({ database }) => {
                     addData={addData} />
                 <div className="start-new-doc">
                     <div>
-                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/docs-blank-googlecolors.png" alt="" onClick={handleOpen}/>
+                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/docs-blank-googlecolors.png" alt="" onClick={handleOpen} />
+                        <div>
+                            <p>Blank</p>
+                            <p></p>
+                        </div>
                     </div>
                     <div>
-                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/1wyFqxsRmKm9q--7j4WRmBMn694YdhV6hmNrfh4rVm2E_400.png" alt="" onClick={handleOpen}/>
+                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/1wyFqxsRmKm9q--7j4WRmBMn694YdhV6hmNrfh4rVm2E_400.png" alt="" onClick={handleOpen} />
+                        <div>
+                            <p>Resume</p>
+                            <p className="light-font">Serif</p>
+                        </div>
                     </div>
                     <div>
-                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/10bJALGfGJG8BrzBSmG6EznIq6-84l1TZkQ-HC8jO368_400.png" alt="" onClick={handleOpen}/>
+                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/10bJALGfGJG8BrzBSmG6EznIq6-84l1TZkQ-HC8jO368_400.png" alt="" onClick={handleOpen} />
+                        <div>
+                            <p>Resume</p>
+                            <p className="light-font">Coral</p>
+                        </div>
                     </div>
                     <div>
-                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/10e8_E36oj6_LuCRzckBFX_9oqbCHntmYB-jxB5U9gsw_400_2.png" alt="" onClick={handleOpen}/>
+                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/10e8_E36oj6_LuCRzckBFX_9oqbCHntmYB-jxB5U9gsw_400_2.png" alt="" onClick={handleOpen} />
+                        <div>
+                            <p>Letter</p>
+                            <p className="light-font">Spearmint</p>
+                        </div>
                     </div>
                     <div>
-                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/1XykI9TfWo4IoUqGLjQ-D8NIU4jZ1Ml9OI8-Euj5FrA0_400_3.png" alt="" onClick={handleOpen}/>
+                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/1XykI9TfWo4IoUqGLjQ-D8NIU4jZ1Ml9OI8-Euj5FrA0_400_3.png" alt="" onClick={handleOpen} />
+                        <div>
+                            <p>Project Proposal</p>
+                            <p className="light-font">Topic</p>
+                        </div>
                     </div>
                     <div>
-                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/1TojfPV3jurwEV2RpmVqnCCCR4z9g2eQBZ40XTHPBqk8_400_3.png" alt="" onClick={handleOpen}/>
+                        <img src="https://ssl.gstatic.com/docs/templates/thumbnails/1TojfPV3jurwEV2RpmVqnCCCR4z9g2eQBZ40XTHPBqk8_400_3.png" alt="" onClick={handleOpen} />
+                        <div>
+                            <p>Brochure</p>
+                            <p className="light-font">Geometric</p>
+                        </div>
                     </div>
                 </div>
                 <div className="grid-main">
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,24 @@ import { getFirestore } from "firebase/firestore"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyBkWiG2htgsvNCxkktNCxAS3jftwlZgTIM",
-    authDomain: "docs-clone-45214.firebaseapp.com",
-    projectId: "docs-clone-45214",
-    storageBucket: "docs-clone-45214.appspot.com",
-    messagingSenderId: "586762180595",
-    appId: "1:586762180595:web:2a8eb8a56737c716202486"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBkWiG2htgsvNCxkktNCxAS3jftwlZgTIM",
+//     authDomain: "docs-clone-45214.firebaseapp.com",
+//     projectId: "docs-clone-45214",
+//     storageBucket: "docs-clone-45214.appspot.com",
+//     messagingSenderId: "586762180595",
+//     appId: "1:586762180595:web:2a8eb8a56737c716202486"
+// };
+
+const firebaseConfig =
+{
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    messagingSenderId: process.env.REACT_APP_STORAGE_BUCKET,
+    appId: process.env.REACT_APP_APP_ID
+}
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
